b068ec3f1093e027729a317987bf3ea8a1fd264f
hdk5 <hdk5@hdk5.xyz>
2026-08-11 19:24:28 +0300

    WIP more tests

--- test/mitmproxy/proxy/test_commands.py
+++ test/mitmproxy/proxy/test_commands.py
@@ -32,3 +32,47 @@ def test_start_hook():
     f = TestHook(b"foo")
     assert f.args() == [b"foo"]
     assert TestHook in all_hooks.values()
+
+
+@pytest.mark.parametrize("command_cls", [commands.RunInThread, commands.Await])
+def test_command_unwrap(command_cls):
+    if command_cls is commands.RunInThread:
+        command = command_cls(pow, 2, exp=3)
+    else:
+
+        async def awaitable(base, exp):
+            return pow(base, exp)
+
+        command = command_cls(awaitable(2, exp=3))
+
+    generator = command.unwrap()
+
+    assert next(generator) is command
+    with pytest.raises(StopIteration) as done:
+        generator.send((8, None))
+    assert done.value.value == 8
+
+    if command_cls is commands.RunInThread:
+        assert command.args == (2,)
+        assert command.kwargs == {"exp": 3}
+
+
+@pytest.mark.parametrize("command_cls", [commands.RunInThread, commands.Await])
+def test_command_unwrap_error(command_cls):
+    error = RuntimeError("test error")
+    if command_cls is commands.RunInThread:
+        command = command_cls(lambda: None)
+    else:
+
+        async def awaitable():
+            return None
+
+        command = command_cls(awaitable())
+
+    generator = command.unwrap()
+    assert next(generator) is command
+    with pytest.raises(RuntimeError, match="test error"):
+        generator.send((None, error))
+
+    if command_cls is commands.Await:
+        command.awaitable.close()
